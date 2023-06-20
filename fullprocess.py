b91351fd213c800a382f6ec2100b154ec8cd8703
dangstan <dangstan@github.com>
2023-06-20 16:49:48 -0300

    last step finished

--- fullprocess.py
+++ fullprocess.py
@@ -1,66 +1,76 @@
-
 import os
-import training
-import scoring
-import deployment
-import diagnostics
-import reporting
+import json
 import pandas as pd
 import pickle
+from scoring import score_model
 
 ##################Load config.json and get environment variables
 with open('config.json','r') as f:
     config = json.load(f) 
 
-prod_deployment_path = os.path.join(config['prod_deployment_path']) 
-source_path = os.path.join(config['sourcedata']) 
+prod_deployment_path = config['prod_deployment_path']
+output_folder_path = config['output_folder_path']
+source_path = config['input_folder_path']
 
-##################Check and read new data
-#first, read ingestedfiles.txt
-file_names = []
-with open(prod_deployment_path+'ingestedfiles.txt', 'r') as f:
-    # Read each line
-    for line in f:
-        # Remove newline characters
-        line = line.strip()
-        # Append the file name to the list
-        file_names.append(line)
+if not os.path.exists(output_folder_path):
+    os.makedirs(output_folder_path)
 
-#second, determine whether the source data folder has files that aren't listed in ingestedfiles.txt
-source_files = os.listdir(os.getcwd()+source_path)
+def pipeline():
 
-if any(x not in file_names for x in source_files):
-    os.system('python3 ingestion.py')
+    ##################Check and read new data
+    #first, read ingestedfiles.txt
+    file_names = []
+    with open(prod_deployment_path+'/ingestedfiles.txt', 'r') as f:
+        # Read each line
+        for line in f:
+            # Remove newline characters
+            line = line.strip()
+            # Append the file name to the list
+            file_names.append(line)
 
+    #second, determine whether the source data folder has files that aren't listed in ingestedfiles.txt
+    source_files = os.listdir(os.getcwd()+'/'+source_path)
 
-##################Deciding whether to proceed, part 1
-#if you found new data, you should proceed. otherwise, do end the process here
-
-
-##################Checking for model drift
-#check whether the score from the deployed model is different from the score from the model that uses the newest ingested data
-with open(prod_deployment_path+'/latestscore.txt', 'rb') as file:
-    score = file.read()   
-with open(prod_deployment_path+'/trainedmodel.pkl', 'rb') as file:
-    model = pickle.load(file)
+    if any(x not in file_names for x in source_files):
+        os.system('python3 ingestion.py')
+    else:
+        print(file_names,source_files)
+        return
 
 
+    ##################Deciding whether to proceed, part 1
+    #if you found new data, you should proceed. otherwise, do end the process here
 
 
-##################Deciding whether to proceed, part 2
-#if you found model drift, you should proceed. otherwise, do end the process here
+    ##################Checking for model drift
+    #check whether the score from the deployed model is different from the score from the model that uses the newest ingested data
+    with open(prod_deployment_path+'/latestscore.txt', 'rb') as file:
+        old_score = file.read()
+    with open(prod_deployment_path+'/trained_model.pkl', 'rb') as file:
+        model = pickle.load(file)
 
+    data = pd.read_csv(output_folder_path+'/finaldata.csv')
 
+    new_score = score_model(model,data)
 
-##################Re-deployment
-#if you found evidence for model drift, re-run the deployment.py script
+    ##################Deciding whether to proceed, part 2
+    #if you found model drift, you should proceed. otherwise, do end the process here
+    print(new_score,old_score)
+    if new_score > float(old_score):
+        print(new_score,old_score)
+        return
 
-##################Diagnostics and reporting
-#run diagnostics.py and reporting.py for the re-deployed model
+    ##################Re-deployment
+    #if you found evidence for model drift, re-run the deployment.py script
+    os.system('python3 training.py')
+    os.system('python3 scoring.py')
+    os.system('python3 deployment.py')
 
+    ##################Diagnostics and reporting
+    #run diagnostics.py and reporting.py for the re-deployed model
+    os.system('python3 apicalls.py')
+    os.system('python3 diagnostics.py')
+    os.system('python3 reporting.py')
 
-
-
-
-
-
+if __name__ == '__main__':
+    pipeline()
--- scoring.py
+++ scoring.py
@@ -2,6 +2,7 @@ import pandas as pd
 import numpy as np
 import pickle
 import os
+import sys
 from sklearn import metrics
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
@@ -20,20 +21,43 @@ model_path = os.path.join(config['output_model_path'])
 
 
 #################Function for model scoring
-def score_model():
+def score_model(model=None,data=None):
     #this function should take a trained model, load test data, and calculate an F1 score for the model relative to the test data
     #it should write the result to the latestscore.txt file
-    test_data = pd.read_csv(test_data_path+'/testdata.csv')
-    with open(model_path+'/trained_model.pkl', 'rb') as file:
-        model = pickle.load(file)
+    if not isinstance(data, pd.DataFrame):
+        data = pd.read_csv(test_data_path+'/testdata.csv')
+    if model!='':
+        try:
+            with open(model_path+'/trained_model.pkl', 'rb') as file:
+                model = pickle.load(file)
+        except:
+            with open('practice'+model_path+'/trained_model.pkl', 'rb') as file:
+                model = pickle.load(file)
 
-    predictions = model.predict(test_data.iloc[:,1:-1])
-    score = f1_score(test_data.iloc[:,-1],predictions)
-    with open(model_path+'/latestscore.txt', 'w') as file:
-        file.write(str(score))
+    predictions = model.predict(data.iloc[:,1:-1])
+    score = f1_score(data.iloc[:,-1],predictions)
+    try:
+        with open(model_path+'/latestscore.txt', 'w') as file:
+            file.write(str(score))
+    except:
+        with open('practice'+model_path+'/latestscore.txt', 'w') as file:
+            file.write(str(score))
 
     return score
 
 
 if __name__ == '__main__':
-    score_model()
+    # Check if command-line arguments are provided
+    if len(sys.argv) >= 3:
+        model_argument = sys.argv[1]
+        data_argument = sys.argv[2]
+
+        # Load the model and test data from arguments
+        with open(model_argument, 'rb') as file:
+            model = pickle.load(file)
+        test_data = pd.read_csv(data_argument)
+
+        # Call the scoring function with the provided model and data
+        score_model(model, test_data)
+    else:
+        score_model()
